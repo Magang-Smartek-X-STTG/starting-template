e2381ef700ac25ff5ca637d97431413a15007de8
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-28 13:28:22 -0700

    Add style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello Word</h1>
+    <div className="container mx-auto bg-red-400">
+      <h1 className="text-2xl">Hello Word</h1>
     </div>
   );
 }
